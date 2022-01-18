7341f7501136367584946815e9b371dd8d06b0f1
premaltailor <premal.tailor@kellogg.northwestern.edu>
2022-01-18 17:27:34 +0000

    Finish Lab 3

--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,48 @@
+-- Drops existing tables, so we start fresh with each
+-- run of this script
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS likes;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS followers;
 
+
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    first_name TEXT,
+    last_name TEXT,
+    location TEXT
+);
+
+
+
+CREATE TABLE followers ( 
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    first_name TEXT,
+    last_name TEXT,
+    location TEXT
+); 
+
+CREATE TABLE likes (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+    self_id INTEGER,
+    followers_id INTEGER,
+    post_id INTEGER
+)
+
+CREATE TABLE comments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    self_id INTEGER,
+    followers_id INTEGER,
+    post_id INTEGER
+)
+
+CREATE TABLE posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    timestamp TEXT,
+    likes INTEGER,
+    comments TEXT.
+);
